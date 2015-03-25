0a484b7bc72ea1c286dba7bfce25ce061375b3af
Vritant Jain <adarshvritant@gmail.com>
2015-03-25 10:57:52 -0400

    format js files angular style

--- app/src/main/webapp/js/esbMessageAdminApp.js
+++ app/src/main/webapp/js/esbMessageAdminApp.js
@@ -1,71 +1,69 @@
-var esbMessageAdminApp = angular.module('esbMessageAdminApp', ['ui.bootstrap', 'ngRoute', 'ngGrid', 'esbMessageAdminControllers', 'esbMessageAdminServices', 'angucomplete', 'angular-loading-bar', 'ngQuickDate', 'MessageCenterModule']);
+var esbMessageAdminApp = angular.module('esbMessageAdminApp', [ 'ui.bootstrap',
+        'ngRoute', 'ngGrid', 'esbMessageAdminControllers',
+        'esbMessageAdminServices', 'angucomplete', 'angular-loading-bar',
+        'ngQuickDate', 'MessageCenterModule' ]);
 
-esbMessageAdminApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/errors', {
-        title: "Errors and Messages",
-        templateUrl: 'partials/errors.html',
-      }).
-      when('/sync', {
-        title: "Sync",
-        templateUrl: 'partials/sync.html',
-      }).
-      when('/synckeys', {
-          title: "Sync Keys",
-          templateUrl: 'partials/synckeys.html',
-        }).
-      when('/searchkeys', {
-            title: "Search Keys",
-            templateUrl: 'partials/searchkeys.html',
-          }).
-      when('/users', {
-          title: "Users",
-          templateUrl: 'partials/users.html',
-        }).
-      otherwise({
-        redirectTo: '/errors'
-      });
-  }]);
-
-esbMessageAdminApp.run(['$location', '$rootScope', function($location, $rootScope) {
-    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
-        if (current.$$route)
-          $rootScope.title = current.$$route.title;
+esbMessageAdminApp.config([ '$routeProvider', function($routeProvider) {
+    $routeProvider.when('/errors', {
+        title : "Errors and Messages",
+        templateUrl : 'partials/errors.html',
+    }).when('/sync', {
+        title : "Sync",
+        templateUrl : 'partials/sync.html',
+    }).when('/synckeys', {
+        title : "Sync Keys",
+        templateUrl : 'partials/synckeys.html',
+    }).when('/searchkeys', {
+        title : "Search Keys",
+        templateUrl : 'partials/searchkeys.html',
+    }).when('/users', {
+        title : "Users",
+        templateUrl : 'partials/users.html',
+    }).otherwise({
+        redirectTo : '/errors'
     });
-}]);
+} ]);
 
-esbMessageAdminApp.provider('Globals', function(){
+esbMessageAdminApp.run([
+        '$location',
+        '$rootScope',
+        function($location, $rootScope) {
+            $rootScope.$on('$routeChangeSuccess', function(event, current,
+                    previous) {
+                if (current.$$route)
+                    $rootScope.title = current.$$route.title;
+            });
+        } ]);
+
+esbMessageAdminApp.provider('Globals', function() {
     var self = this;
 
     // providers are initialized before services
     // this guarantees that Globals is setup first
-    self.$get = ['$window', function($window) {
+    self.$get = [ '$window', function($window) {
 
         var globals = {
-            'dateFormat': {
-                'service': 'yyyy-MM-ddTHH:mm:ss',
-                'datepicker': 'yyyy/MM/dd',
-                'timepicker': 'HH:mm:ss'
+            'dateFormat' : {
+                'service' : 'yyyy-MM-ddTHH:mm:ss',
+                'datepicker' : 'yyyy/MM/dd',
+                'timepicker' : 'HH:mm:ss'
             },
-            'serverSideLogging': {
-                'info': false,
-                'debug': false,
-                'warn': true,
-                'error': true
+            'serverSideLogging' : {
+                'info' : false,
+                'debug' : false,
+                'warn' : true,
+                'error' : true
             }
         };
 
         return globals;
 
-    }];
+    } ];
 });
 
-String.prototype.supplant = function (o) {
-    return this.replace(/{([^{}]*)}/g,
-        function (a, b) {
-            var r = o[b];
-            return typeof r === 'string' || typeof r === 'number' ? r : a;
-        }
-    );
+String.prototype.supplant = function(o) {
+    return this.replace(/{([^{}]*)}/g, function(a, b) {
+        var r = o[b];
+        return typeof r === 'string' || typeof r === 'number' ? r : a;
+    });
 };
--- app/src/test/js/errorsSearchCtrlTest.js
+++ app/src/test/js/errorsSearchCtrlTest.js
@@ -1,66 +1,84 @@
-describe('ErrorsSearchCtrl', function() {
+describe(
+        'ErrorsSearchCtrl',
+        function() {
 
-  // Instantiate a new version of my module before each test
-  beforeEach(module('esbMessageAdminApp'));
+            // Instantiate a new version of my module before each test
+            beforeEach(module('esbMessageAdminApp'));
 
-  var errorsSearchCtrl, http, globals, scope, rootScope, esbMessageService, filter;
+            var errorsSearchCtrl, http, globals, scope, rootScope, esbMessageService, filter;
 
-  var toDate, fromDate, toDateStr, fromDateStr;
+            var toDate, fromDate, toDateStr, fromDateStr;
 
-  var noResultsResponse = {"totalResults":0,"itemsPerPage":0,"page":0,"messages":null};
+            var noResultsResponse = {
+                "totalResults" : 0,
+                "itemsPerPage" : 0,
+                "page" : 0,
+                "messages" : null
+            };
 
-  beforeEach(inject(function($rootScope, $controller, $httpBackend, Globals, EsbMessageService, $filter) {
-    rootScope = $rootScope;
-    scope = $rootScope.$new();
-    http = $httpBackend;
-    globals = Globals;
-    esbMessageService = EsbMessageService;
-    filter = $filter;
+            beforeEach(inject(function($rootScope, $controller, $httpBackend,
+                    Globals, EsbMessageService, $filter) {
+                rootScope = $rootScope;
+                scope = $rootScope.$new();
+                http = $httpBackend;
+                globals = Globals;
+                esbMessageService = EsbMessageService;
+                filter = $filter;
 
-    toDate = new Date(2015, 0, 15, 0, 0, 0);
+                toDate = new Date(2015, 0, 15, 0, 0, 0);
 
-    fromDate = new Date(toDate.getTime());
-    fromDate.setDate(fromDate.getDate() -1); // yesterday
+                fromDate = new Date(toDate.getTime());
+                fromDate.setDate(fromDate.getDate() - 1); // yesterday
 
-    toDateStr = filter('date')(toDate, globals.dateFormat.service);
-    fromDateStr = filter('date')(fromDate, globals.dateFormat.service);
+                toDateStr = filter('date')(toDate, globals.dateFormat.service);
+                fromDateStr = filter('date')(fromDate,
+                        globals.dateFormat.service);
 
-    //spyOn(MessageService, 'addFailureMessage').andCallThrough();
-    //spyOn(termsService, 'updateTerm').andCallThrough();
+                // spyOn(MessageService, 'addFailureMessage').andCallThrough();
+                // spyOn(termsService, 'updateTerm').andCallThrough();
 
-    (errorsSearchCtrl) = $controller('ErrorsSearchCtrl', {'$scope': scope});
+                (errorsSearchCtrl) = $controller('ErrorsSearchCtrl', {
+                    '$scope' : scope
+                });
 
-    // not testing autcompletion stuff - those unit tests belong to angucomplete-keyvalue project
-    http.expectGET("api/search/suggest/").respond(200);
-    // Simulate a server response
-    http.flush();
-  }));
+                // not testing autcompletion stuff - those unit tests belong to
+                // angucomplete-keyvalue project
+                http.expectGET("api/search/suggest/").respond(200);
+                // Simulate a server response
+                http.flush();
+            }));
 
-  it('makes a GET request with parameters built from search criteria', function() {
+            it(
+                    'makes a GET request with parameters built from search criteria',
+                    function() {
 
-    rootScope.searchField_searchStr = 'Key="Value"; Blah=" Bleh ";   Foo="bar"  ; invalid';
-    scope.toDate = toDate;
-    scope.fromDate = fromDate;
+                        rootScope.searchField_searchStr = 'Key="Value"; Blah=" Bleh ";   Foo="bar"  ; invalid';
+                        scope.toDate = toDate;
+                        scope.fromDate = fromDate;
 
-    scope.search();
+                        scope.search();
 
-    var url = "api/search/criteria/crit{crit}?fromDate={fromDate}&toDate={toDate}&start=0&results=12&sortField=timestamp&sortAsc=true".
-      supplant({crit: ';Key=Value;Blah=Bleh;Foo=bar', fromDate: fromDateStr, toDate: toDateStr});
+                        var url = "api/search/criteria/crit{crit}?fromDate={fromDate}&toDate={toDate}&start=0&results=12&sortField=timestamp&sortAsc=true"
+                                .supplant({
+                                    crit : ';Key=Value;Blah=Bleh;Foo=bar',
+                                    fromDate : fromDateStr,
+                                    toDate : toDateStr
+                                });
 
-    http.expectGET(url).respond(200, noResultsResponse);
-    // Simulate a server response
-    http.flush();
+                        http.expectGET(url).respond(200, noResultsResponse);
+                        // Simulate a server response
+                        http.flush();
 
-  });
+                    });
 
-  afterEach(function() {
-    // Ensure that all expects set on the $httpBackend
-    // were actually called
-    http.verifyNoOutstandingExpectation();
+            afterEach(function() {
+                // Ensure that all expects set on the $httpBackend
+                // were actually called
+                http.verifyNoOutstandingExpectation();
 
-    // Ensure that all requests to the server
-    // have actually responded (using flush())
-    http.verifyNoOutstandingRequest();
-  });
+                // Ensure that all requests to the server
+                // have actually responded (using flush())
+                http.verifyNoOutstandingRequest();
+            });
 
-});
+        });
