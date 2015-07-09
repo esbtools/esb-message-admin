f084a1c3fa61d107341f74bdc94096fbe87f2998
Alec Henninger <ahenning@redhat.com>
2015-07-09 09:33:03 -0400

    Format karma configs

--- app/src/test/resources/karma.conf.gui.js
+++ app/src/test/resources/karma.conf.gui.js
@@ -1,11 +1,11 @@
 var baseConfig = require('./karma.conf.headless.js');
 module.exports = function(config) {
-    // Load base config
-    baseConfig(config);
-    // Override base config
-    config.set({
-        singleRun : false,
-        autoWatch : true,
-        browsers : [ 'Chrome', 'Firefox' ]
-    });
+  // Load base config
+  baseConfig(config);
+  // Override base config
+  config.set({
+    singleRun: false,
+    autoWatch: true,
+    browsers: ['Chrome', 'Firefox']
+  });
 };
