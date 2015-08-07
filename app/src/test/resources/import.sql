e86f278bdd5e625b2b3d375d6ed48ae8c7df0d82
Kevin Howell <khowell@redhat.com>
2015-08-07 08:57:18 -0400

    Add jetty config

---
+++ app/src/test/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO METADATA (id, name, parent_id, type, value) values (1, 'SearchKeys', -1, 'SearchKeys', 'SearchKeys');
+INSERT INTO METADATA (id, name, parent_id, type, value) values (2, 'Entities', -1, 'Entities', 'Entities');
