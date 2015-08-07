836170e157ad37fb96811cd2afa0f57301f979de
Kevin Howell <khowell@redhat.com>
2015-08-07 11:41:56 -0400

    Use exampleDataLoad.sql as import.sql via symlink
    Required a bit of cleanup - removing backticks and select statements
    from the script.
    
    This allows jetty to share the example data :-)

--- app/src/test/resources/import.sql
+++ app/src/test/resources/import.sql
@@ -1,2 +1,1 @@
-INSERT INTO METADATA (id, name, parent_id, type, value) values (1, 'SearchKeys', -1, 'SearchKeys', 'SearchKeys');
-INSERT INTO METADATA (id, name, parent_id, type, value) values (2, 'Entities', -1, 'Entities', 'Entities');
+../../../../db/exampleDataLoad.sql
