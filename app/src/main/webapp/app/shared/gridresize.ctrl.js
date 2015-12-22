7421e979afc681b00792839beb16e73dc4394268
Benjamin Miller <bmiller@redhat.com>
2015-12-22 10:45:28 -0500

    Factoring out grid resize functionality

---
+++ app/src/main/webapp/app/shared/gridresize.ctrl.js
@@ -0,0 +1,16 @@
+esbMessageAdminApp.controller('gridResizeController',
+	[
+	    '$scope',
+	    '$rootScope',
+	    '$element',
+	    function ($scope, $rootScope, $element) {
+	        $scope.$watch(
+	        	function() {
+		            return $element.width();
+		        }, function() {
+		            $rootScope.$broadcast('errorGridResize');
+		        }
+	        );
+	    }
+	]
+);
