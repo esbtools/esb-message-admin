fde44b6fae1a8016366339fbe4dd7867ebe1fb27
Matthew Stratton <mstratto@redhat.com.com>
2016-03-01 15:04:36 -0500

    adding some much needed logging and error handling to ema resubmit logic

--- common/src/main/java/org/esbtools/message/admin/common/feature/EmaResubmit.java
+++ common/src/main/java/org/esbtools/message/admin/common/feature/EmaResubmit.java
@@ -1,6 +1,7 @@
 package org.esbtools.message.admin.common.feature;
 
 import static org.esbtools.message.admin.common.config.EMAConfiguration.getEditableMessageTypes;
+import static org.esbtools.message.admin.common.config.EMAConfiguration.getResubmitBlackList;
 import static org.esbtools.message.admin.common.config.EMAConfiguration.getResubmitControlHeader;
 import static org.esbtools.message.admin.common.config.EMAConfiguration.getResubmitHeaderNamespace;
 import static org.esbtools.message.admin.common.config.EMAConfiguration.getResubmitRestEndpoints;
@@ -13,6 +14,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
+import org.apache.commons.lang3.StringUtils;
 import org.esbtools.gateway.resubmit.ResubmitRequest;
 import org.esbtools.message.admin.common.orm.AuditEventEntity;
 import org.esbtools.message.admin.common.orm.EsbMessageEntity;
@@ -33,10 +35,6 @@ public class EmaResubmit {
     @Inject
     private EntityManager entityMgr;
     
-    public EmaResubmit(){
-        
-    }
-    
     public MetadataResponse resubmitMessage(EsbMessage esbMessage) {
 
         ResubmitRequest request = new ResubmitRequest();
@@ -47,14 +45,17 @@ public class EmaResubmit {
             throw new IllegalArgumentException("Message {\n}"+esbMessage.toString()+"\n} does not exist in backend store, cannot update.");
         }
 
-
+        if(StringUtils.isBlank(getResubmitControlHeader())){
+            throw new RuntimeException("resubmitControlHeader not set in config.json - resubmit feature is unusable");
+        }
+        
         // It may be the case that the header we're configured to use isn't
         // present on the message So we should indicate this with an appropriate
-        // return object so the front end can act.
+        // return object so the front end can act.        
         try{
             request.setDestination( persistedMessage.getHeader( getResubmitControlHeader() ).getValue() );
         }catch(Exception e){
-            LOG.warn("Warning: Message that was resubmitted lacked configured control header! Message ID: " + new Long(esbMessage.getId()).toString() );
+            LOG.warn("Message that was resubmitted lacked configured control header! Message ID: " + new Long(esbMessage.getId()).toString() );
             MetadataResponse result = new MetadataResponse();
             result.setErrorMessage("Unable to resubmit message due to configured resubmit control header not being present on message.");
             return result;
@@ -63,7 +64,6 @@ public class EmaResubmit {
         request.setSystem( persistedMessage.getSourceSystem() );
 
         // explicitly check if the loaded message is in our list of allowed message types
-        LOG.warn(esbMessage.toString());
         if( isEditableMessage(persistedMessage) ){
             request.setPayload( esbMessage.getPayload() );
         } else {
@@ -87,6 +87,9 @@ public class EmaResubmit {
     }
     
     private MetadataResponse sendMessageToResubmitGateway( String message ) {
+        if(getResubmitRestEndpoints().size() == 0){
+            throw new RuntimeException("resubmitRestEndpoints not set in config.json - resubmit feature is unusable");
+        }
 
         if( RestRequestUtility.sendMessageToRestEndPoint( message, getResubmitRestEndpoints() ) ){
             return new MetadataResponse();
@@ -98,16 +101,59 @@ public class EmaResubmit {
 
     }
     
-    private Boolean isEditableMessage( EsbMessageEntity message ){
+    public static Boolean isEditableMessage( EsbMessageEntity message ){
         if(message.getMessageType() == null){
+            LOG.warn(message.getMessageId()  + " not editable - has no message type, defaulting to not allowing editing");
             return false;
         }
         
-        return getEditableMessageTypes().contains( message.getMessageType().toUpperCase() );
+        if(!getEditableMessageTypes().contains(message.getMessageType().toUpperCase())){
+            LOG.info(message.getMessageId()  + " not editable - " + message.getMessageType() + " is not an editable message type");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public static Boolean allowsResubmit( EsbMessage message ){
+        if(message.getMessageType() == null){
+            LOG.warn(message.getMessageId()  + " not resubmitted - has no message type, defaulting to not allowing resubmit");
+            return false;
+        }
+        
+        if(message.getResubmittedOn() != null){
+            LOG.info(message.getMessageId() + " not resubmitted - already resubmitted on " + message.getResubmittedOn().toString());
+            return false;
+        }
+        
+        if(getResubmitBlackList().contains( message.getMessageType().toUpperCase() )){
+            LOG.info(message.getMessageId()  + " not resubmitted - message type " + message.getMessageType() + " is in the blacklist");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public static Boolean isEditableMessage( EsbMessage message ){
+        if(message.getMessageType() == null){
+            LOG.warn(message.getMessageId() + " has no message type, defaulting to not allowing editing");
+            return false;
+        }
+        
+        if(!getEditableMessageTypes().contains(message.getMessageType().toUpperCase())){
+            LOG.info(message.getMessageId()  + " not editable - " + message.getMessageType() + " is not an editable message type");
+            return false;
+        }
+        
+        return true;
     }
     
     private Map<String,String> reduceToEsbHeaders( EsbMessageEntity message ){
         Map<String,String> headers = new HashMap<String,String>();
+        
+        if(StringUtils.isBlank(getResubmitHeaderNamespace())){
+            LOG.warn("Resubmit header namespace is blank, this means no headers will be added to the resubmit message");
+        }
 
         for( EsbMessageHeaderEntity header : message.getErrorHeaders() ){
             if( header.getName().contains( getResubmitHeaderNamespace() ) ){
