eda8f297d67e56273bc219de3b3bc59e54fcecc6
Matthew Stratton <mstratto@redhat.com.com>
2016-03-01 16:44:32 -0500

    refactoring emaresync to use ResyncRequest obejct rather than constructing its own json

--- common/src/main/java/org/esbtools/message/admin/common/feature/EmaResync.java
+++ common/src/main/java/org/esbtools/message/admin/common/feature/EmaResync.java
@@ -2,10 +2,13 @@ package org.esbtools.message.admin.common.feature;
 
 import static org.esbtools.message.admin.common.config.EMAConfiguration.getResyncRestEndpoints;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
+import org.esbtools.gateway.resync.ResyncRequest;
 import org.esbtools.message.admin.common.orm.AuditEventEntity;
 import org.esbtools.message.admin.common.utility.RestRequestUtility;
 import org.esbtools.message.admin.model.AuditEvent;
@@ -23,50 +26,23 @@ public class EmaResync {
     private EntityManager entityMgr;
     
     public MetadataResponse syncMessage(String entity, String system, String key, String... values) {
-        String resyncMessage = createResyncMessage(entity, system, key, values);
+        ResyncRequest resyncMessage = new ResyncRequest();
+        resyncMessage.setEntity(entity);
+        resyncMessage.setSystem(system);
+        resyncMessage.setKey(key);
+        resyncMessage.setValues(Arrays.asList(values));
         
-        LOG.info("Initiating sync request: {}", resyncMessage);
+        LOG.info("Initiating sync request: {}", resyncMessage.toJson());
     
-        saveAuditEvent(new AuditEvent(DEFAULT_USER, "SYNC", METADATA_KEY_TYPE, entity, key, resyncMessage));
+        saveAuditEvent(new AuditEvent(DEFAULT_USER, "SYNC", METADATA_KEY_TYPE, entity, key, resyncMessage.toJson()));
     
-        if( RestRequestUtility.sendMessageToRestEndPoint( resyncMessage, getResyncRestEndpoints() ) ){
+        if( RestRequestUtility.sendMessageToRestEndPoint( resyncMessage.toJson(), getResyncRestEndpoints() ) ){
             return new MetadataResponse();
         }else{
             MetadataResponse result = new MetadataResponse();
             result.setErrorMessage("Unable to resync message");
             return result;
         }
-    }
-    
-    private static String createResyncMessage(String entity, String system, String key, String... values){
-        StringBuilder message = new StringBuilder("{");
-        message.append("\"entity\" : \"");
-        message.append(entity);
-        message.append("\",");
-        message.append("\"system\" : \"");
-        message.append(system);
-        message.append("\",");
-        message.append("\"key\": \"");
-        message.append(key);
-        message.append("\",");
-        message.append("\"values\" : [");
-
-        int i = 0;
-        for(String value: values) {
-            if(value!=null && value.length()>0) {
-                if(i>0) {
-                    message.append(",");
-                }
-                message.append("\"");
-                message.append(value);
-                message.append("\"");
-            }
-            i++;
-        }
-        message.append("]");
-        message.append("}");
-        
-        return message.toString();
     }
     
     /**
